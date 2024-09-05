5c11beeaca2a5a613c51549e8d17ede267971af1
dearxcorex <deardevx@gmail.com>
2024-09-05 22:50:55 +0700

    filter frequency

--- bot.py
+++ bot.py
@@ -14,15 +14,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
-
 #AI 
-import pandas as pd 
-from langchain.agents.agent_types import AgentType
-from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain_openai import ChatOpenAI
-
-
+from search_frequency import frequency_search
 
 # Set up logging 
 load_dotenv()
@@ -34,15 +27,6 @@ logger = logging.getLogger(__name__)
 OPENAI_API = os.getenv("OPENAI_API_KEY")
 
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-# Define states
-WAITING_FOR_FREQUENCY = 1
-
-
-#set up  openai and langchain 
-chat = ChatOpenAI(api_key=OPENAI_API,model='gpt-4o',temperature=0.0)
-df = pd.read_csv("frequency_analysis/merged.csv",index_col=0)
-agent = create_pandas_dataframe_agent(chat,df,agent_type=AgentType.OPENAI_FUNCTIONS,verbose=True,allow_dangerous_code=True)
-
 
 async def google_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     creds = None 
@@ -96,27 +80,6 @@ async def google_calendar(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         await update.message.reply_text("An error occurred while accessing google calendar.")
 
 
-
-
-async def start_search_frequency(update:Update,context:ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text("Please enter the frequency you want to search for :")
-    return WAITING_FOR_FREQUENCY
-
-async def find_frequency_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
-
-
-    message_text = update.message.text
-    response = agent.invoke(f"Find rows where 'คลื่นความถี่ (MHz)' is {message_text}")
-    final = response['output']
-    
-    await context.bot.send_message(chat_id=update._effective_chat.id, text=final,parse_mode="Markdown")
-    return ConversationHandler.END
-
-async def end(update:Update,context:ContextTypes.DEFAULT_TYPE) ->int:
-    await update.message.reply_text("Search canceled.")
-    return ConversationHandler.END
-
 def main():
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN") 
     if not bot_token:
@@ -126,11 +89,11 @@ def main():
     app = ApplicationBuilder().token(bot_token).build()
     app.add_handler(CommandHandler("task", google_calendar))
     conv_hanler = ConversationHandler(
-        entry_points=[CommandHandler('freq',start_search_frequency)],
+        entry_points=[CommandHandler('freq',frequency_search.start_search_frequency)],
         states={
-            WAITING_FOR_FREQUENCY:[MessageHandler(filters.TEXT & ~ filters.COMMAND,find_frequency_bot)]
+            frequency_search.WAITING_FOR_FREQUENCY:[MessageHandler(filters.TEXT & ~ filters.COMMAND,frequency_search.find_frequency_bot)]
         },
-        fallbacks=[CommandHandler('end',end)]
+        fallbacks=[CommandHandler('end',frequency_search.end)]
     )
     app.add_handler(conv_hanler)
     # app.add_handler(CommandHandler("freq",find_frequency_bot))
